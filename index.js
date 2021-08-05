713071d2086bb252d991f054ef910a91fd7d289a
kccrtv <kimcincocreative@gmail.com>
2021-08-04 22:12:22 -0500

    initial express route set up

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+// instantiate express
+const app = express();
+
+// controllers
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+app.set('port', process.env.PORT || 8000);
+
+app.listen(app.get('port'), () => {
+	console.log(`PORT: ${app.get('port')} ✨`);
+});
