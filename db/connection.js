344bbd250bf6a053198af6b17ba56a4a7cb0de58
kccrtv <kimcincocreative@gmail.com>
2021-08-11 13:48:23 -0500

    configure atlas

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,17 @@
 // importing mongoose to interface with mongoDB
 const mongoose = require('mongoose');
+const URI = process.env.URI;
 
 // store environment variable to set in production, otherwise use the db on local machine
 const dbName = 'employee-directory';
 
 const mongoURI =
-	process.env.NODE_ENV === 'production'
-		? process.env.DB_URL
-		: 'mongodb://localhost/' + dbName;
+	process.env.NODE_ENV === 'production' ? process.env.DB_URL : URI;
+
+// const mongoURI =
+// 	process.env.NODE_ENV === 'production'
+// 		? process.env.DB_URL
+// 		: 'mongodb://localhost/' + dbName;
 
 // add options for the connection as object in second arg, replace local db address with db URI
 mongoose
