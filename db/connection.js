b5caee52c48023d4095e98d220b964a97a34e638
kccrtv <kimcincocreative@gmail.com>
2021-08-11 15:41:47 -0500

    update uri

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,14 @@
 // importing mongoose to interface with mongoDB
 const mongoose = require('mongoose');
-const URI = process.env.URI;
-
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
 // store environment variable to set in production, otherwise use the db on local machine
 const dbName = 'employee-directory';
 
 const mongoURI =
 	process.env.NODE_ENV === 'production'
-		? process.env.DB_URL === `${URI}`
+		? process.env.DB_URL ===
+		  `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.mvgel.mongodb.net/employee-directory?retryWrites=true&w=majority`
 		: 'mongodb://localhost/' + dbName;
 
 // add options for the connection as object in second arg, replace local db address with db URI
